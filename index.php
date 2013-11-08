2aad4102e9e70a5378d9a9c27d033d7fe027daef
sstickl <sstickl@clemson.edu>
2013-11-08 16:21:36 -0500

    Fixed index page
    i suck

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php	header( "Location: page.php?page=index.php" );
 /*what we want to do here is just redirect to the page controller,
 	so we don't have to have two copies of the same page to manage.	*/
-	//$_GET['page'] = 'index.php';
+	//$_GET['page'] = 'index.html';
  //include( 'page.php' ); ?>-->
  
