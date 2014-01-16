0decf25f9e0ca7ee840ac0cc72eedcafb34587c5
sstickl <sstickl@clemson.edu>
2014-01-15 21:54:23 -0500

    Calender page addition
    Calender page added at request of comms group.  Merged Contact Us page
    with Donations page, placed Calender where the old donations page was.

--- navbar.php
+++ navbar.php
@@ -9,8 +9,8 @@
                                         <li><a href="page.php?page=classroom" id="classroom">In The Classroom</a></li>
                                         <li><a href="page.php?page=intern" id="intern">Intern Program</a></li>
                                         <li><a href="page.php?page=trips" id="trips">Trips</a></li>
-                                        <li><a href="page.php?page=donate" id="donate">Donate</a></li>
-                                        <li><a href="page.php?page=contact" id="contact">Contact</a></li>
+                                        <li><a href="page.php?page=calender" id="donate">Calender</a></li>	<!--"donate" because we never bothered to change the script when merging contact/donate pages-->
+                                        <li><a href="page.php?page=contact" id="contact">Contact/Donate</a></li>
                                         <li><a href="page.php?page=gallery" id="gallery">Gallery</a></li>
                                 </ul>
                         </strong>					
