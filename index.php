5c6cd890cfabecf9bd050be484026d5a1d2c16b2
sstickl <sstickl@clemson.edu>
2014-01-15 21:20:12 -0500

    Removed stray end comment

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 	/*what we want to do here is just redirect to the page controller, 
 		so we don't have to have two copies of the same page to manage.	*/ 
 	header( "Location: page.php?page=index" );
-?>-->
+?>
  
