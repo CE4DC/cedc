534820c7f6432e776f1191ca21f18e10a480f77f
nwilli4 <nwilli4@g.clemson.edu>
2014-02-07 15:34:02 -0500

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,7 +1,9 @@
 <div id="nav">
     <div class="container">
-        <ul id="nav-list" style="margin:0 auto;">	<!--this centers the UL, but not the LI... workarounds?-->
-            <li><a href="page.php?page=index&amp;name=Home" id="home" style="margin:50 auto;">Home</a></li>
+        <ul id="nav-list" style="margin:0 auto;">
+        <!--this centers the UL, but not the LI... workarounds?-->
+        <li id="nav-list" style="margin:0 auto;">
+            <li><a href="page.php?page=index&amp;name=Home" id="home">Home</a></li>
 			<li><a href="page.php?page=about&amp;name=About%20Us" id="about">About Us</a></li>
 			<li><a href="page.php?page=projects&amp;name=Projects" id="projects">Projects</a></li>
 			<li><a href="page.php?page=classroom&amp;name=In%20the%20Classroom" id="classroom">In The Classroom</a></li>
