11ed59a59dce57f9794ee35aeb771e1b418ef631
aephynsa <aephynsa@gmail.com>
2014-03-11 09:08:08 -0400

    Reworked header and footer
    Now based almost entirely on small images (~500 bytes) to avoid gradient compatibility issues

--- footer.php
+++ footer.php
@@ -1,4 +1,34 @@
-<div id="footer">
-	<a href="https://sites.google.com/a/g.clemson.edu/cedc/?pli=1" style="color:#FFFFFF">Member Tools</a>
-	<p>© 2013 Clemson Engineers for Developing Countries<br/>Site best viewed in an HTML5 complaint browser such as Firefox 4 in a widescreen resolution over 1600x900.</p>
+</div></div>
+<div id="footer-bg">
+	<div id="footer">
+    <div id="footer-content">
+        <div id="footer-social">
+        	<a href="https://sites.google.com/a/g.clemson.edu/cedc/?pli=1">
+            	<span style="float: left"><img src="img/tools.png" alt="Tools"/></span>
+            </a>
+            <a href="https://www.facebook.com/cedc09"><img src="img/fb.png" alt="Facebook"/></a>
+            <a href="https://twitter.com/CU_CEDC"><img src="img/tw.png" alt="Twitter"/></a>
+            <a href="http://www.linkedin.com/groups/Clemson-Engineers-Developing-Countries-4637840"><img src="img/in.png" alt="Linkedin"/></a>
+        </div>
+        <div id="footer-nav">
+            <ul>
+                <li><a href="page.php?page=index&amp;name=Home">Home</a></li>
+                <li><a href="page.php?page=about&amp;name=About%20Us">About Us</a></li>
+                <li><a href="page.php?page=projects&amp;name=Projects">Projects</a></li>
+                <li><a href="page.php?page=classroom&amp;name=In%20the%20Classroom">In The Classroom</a></li>
+                <li><a href="page.php?page=intern&amp;name=Our%20Interns">Intern Program</a></li>
+                <li><a href="page.php?page=trips&amp;name=Our%20Trips">Trips</a></li>
+                <li><a href="page.php?page=calendar&amp;name=Calendar">Calendar</a></li>
+                <li><a href="page.php?page=contact&amp;name=Contact%20Us">Contact</a></li>
+                <li><a href="page.php?page=gallery&amp;name=Gallery">Gallery</a></li>
+            </ul>
+        </div>
+        <div id="footer-copy">
+            <p>
+            	&copy; 2014 Clemson Engineers for Developing Countries
+            </p>
+        </div>
+    </div>
+	</div>
+</div>
 </div>
--- header.php
+++ header.php
@@ -5,22 +5,30 @@
 
 <link rel="stylesheet" href="./css/style.css">
 
-<body>
-
-<div id="header">      
-    <img id="logo" class="left" src="./img/logo.png" alt="cedc logo"/>
-	<div id="header-text" class="right">Clemson Engineers for Developing Countries<br/>Serving the developing world, Developing the students who serve</div>
-	<div id="nav" class="right">
-        <ul id="nav-list">
-            <li><a href="page.php?page=index&amp;name=Home" id="home">Home</a></li>
-			<li><a href="page.php?page=about&amp;name=About%20Us" id="about">About Us</a></li>
-			<li><a href="page.php?page=projects&amp;name=Projects" id="projects">Projects</a></li>
-			<li><a href="page.php?page=classroom&amp;name=In%20the%20Classroom" id="classroom">In The Classroom</a></li>
-			<li><a href="page.php?page=intern&amp;name=Our%20Interns" id="intern">Intern Program</a></li>
-			<li><a href="page.php?page=trips&amp;name=Our%20Trips" id="trips">Trips</a></li>
-           	<li><a href="page.php?page=calender&amp;name=Calender" id="donate">Calender</a></li>	<!--"donate" because we never bothered to change the script when merging contact/donate pages-->
-			<li><a href="page.php?page=contact&amp;name=Contact%20Us" id="contact">Contact/Donate</a></li>
-           	<li><a href="page.php?page=gallery&amp;name=Gallery" id="gallery">Gallery</a></li>
-       	</ul>
-    </div>
+<div id="wrapper">
+<div id="header-bg">
+	<div id="header">
+		<div id="header-logo">
+        	<img src="./img/logo.png" alt="CEDC logo"/>
+        </div>
+		<div id="header-motto">
+        	Serving&nbsp;the&nbsp;developing world
+            &nbsp;-&nbsp;
+            Developing&nbsp;the&nbsp;students&nbsp;who&nbsp;serve
+		</div>
+        <div id="header-nav">
+        	<ul>
+                <li><a href="page.php?page=index&amp;name=Home"><p>Home</p></a></li>
+                <li><a href="page.php?page=about&amp;name=About%20Us"><p>About<br>Us</p></a></li>
+                <li><a href="page.php?page=projects&amp;name=Projects"><p>Projects</p></a></li>
+                <li><a href="page.php?page=classroom&amp;name=In%20the%20Classroom"><p>In The<br>Classroom</p></a></li>
+                <li><a href="page.php?page=intern&amp;name=Our%20Interns"><p>Intern<br>Program</p></a></li>
+                <li><a href="page.php?page=trips&amp;name=Our%20Trips"><p>Trips</p></a></li>
+                <li><a href="page.php?page=calendar&amp;name=Calendar"><p>Calendar</p></a></li>
+                <li><a href="page.php?page=contact&amp;name=Contact%20Us"><p>Contact</p></a></li>
+                <li><a href="page.php?page=gallery&amp;name=Gallery"><p>Gallery</p></a></li>
+            </ul>
+        </div>
+	</div>
 </div>
+<div id="content"><div id="content-body">
